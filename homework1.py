4694033cf6c9014f4624f1b6e9945fad26c6f4c4
RADARIAS <androks73@mail.ru>
2021-06-26 16:51:14 +0300

    Домашнее задание по первой лекции.

---
+++ homework1.py
@@ -0,0 +1,5 @@
+a = 123
+b = 'hello world !!!'
+user_numbers = int(input('Введите число:\n'))
+user_text = input('Введите имя и фамилию:\n')
+print(f'Пользователь создал переменные {a} и {b}, а так же запросил {user_numbers} и {user_text}')
---
+++ homework3.py
@@ -0,0 +1,4 @@
+n = input('Введите какое нибудь число:\n')
+meaning_nn, meaning_nnn = int(n + n), int(n + n + n)
+n_sum = int(n) + meaning_nn + meaning_nnn
+print(n_sum)
